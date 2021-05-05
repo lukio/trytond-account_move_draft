be2798a61f05728cc65fc4ec2c35d992306c99e0
Raimon Esteve <raimonesteve@gmail.com>
2021-05-05 12:28:45 +0200

    Not allow delete moves when origin is not draft
    #043842

--- tests/test_account_move_draft.py
+++ tests/test_account_move_draft.py
@@ -2,10 +2,11 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 import unittest
-
-
+import doctest
+import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import suite as test_suite
+from trytond.tests.test_tryton import doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class AccountMoveDraftTestCase(ModuleTestCase):
@@ -14,7 +15,11 @@ class AccountMoveDraftTestCase(ModuleTestCase):
 
 
 def suite():
-    suite = test_suite()
+    suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             AccountMoveDraftTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_invoice.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
