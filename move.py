15ba78e69cae965813a5db625a5b1f70b884b7b8
Bernat Brunet <bernat@nan-tic.com>
2023-09-22 11:17:05 +0200

    Change the way that the account move can set to draft. Only accept to set to draft account moves that hasn't an origin, so they have created manually.
    Task: #047268

--- move.py
+++ move.py
@@ -1,18 +1,19 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta, Pool
 from trytond.model import ModelView
 from trytond.pyson import Eval
-from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
 from trytond.i18n import gettext
 from trytond.exceptions import UserError
-from trytond.transaction import Transaction
-
-
-__all__ = ['Move']
 
 
 class Move(metaclass=PoolMeta):
     __name__ = 'account.move'
+
+    allow_draft = fields.Function(
+        fields.Boolean("Allow Draft Move"), 'get_allow_draft')
 
     @classmethod
     def __setup__(cls):
@@ -21,31 +22,36 @@ class Move(metaclass=PoolMeta):
             cls._check_modify_exclude.append('state')
         cls._buttons.update({
             'draft': {
-                'invisible': Eval('state') == 'draft',
+                'invisible': ~Eval('allow_draft', False),
+                'depends': ['allow_draft'],
                 },
             })
+
+    def get_allow_draft(self, name):
+        if self.state == 'draft' or self.origin is not None:
+            return False
+        lines = [line for line in self.lines if line.origin is not None]
+        if lines:
+            return False
+        return True
 
     @classmethod
     @ModelView.button
     def draft(cls, moves):
         Line = Pool().get('account.move.line')
+
+        # Add the possiblity to allow draft a move event it has an origin.
+        # In some special cases is needed.
+        allow_draft = Transaction().context.get('move_allow_draft', False)
+        if not allow_draft:
+            for move in moves:
+                if not move.allow_draft:
+                    raise UserError(gettext(
+                            'account_move_draft.msg_draft_not_allowed',
+                            move=move.number,
+                            ))
+
         cls.write(moves, {
             'state': 'draft',
             })
         Line.check_modify([l for m in moves for l in m.lines])
-
-    @classmethod
-    def delete(cls, moves):
-        if not Transaction().context.get('draft_invoices', False):
-            invoices = [move for move in moves if move.origin
-                and not isinstance(move.origin, str)
-                and move.origin.__name__ == 'account.invoice'
-                and move.origin.state != 'draft']
-
-            if invoices:
-                names = ', '.join(m.rec_name for m in invoices[:5])
-                if len(invoices) > 5:
-                    names += '...'
-                raise UserError(gettext('account_move_draft.msg_delete_moves_invoice',
-                    moves=names))
-        super(Move, cls).delete(moves)
